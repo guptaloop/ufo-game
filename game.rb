f33398949f98aed334c4f0601fadc7c3c2275ce1
Abhi Gupta <abhifgupta@gmail.com>
2020-01-27 14:12:41 -0800

    create initial functionality for render - user sees game greeting and instrux and the UFO -- created UFO Module

--- game.rb
+++ game.rb
@@ -1,13 +1,15 @@
 require 'set'
+require_relative 'assets/ufo'
 
 class Game
-  attr_reader :codeword, :alphabet, :guessed
+  attr_reader :codeword, :alphabet, :guessed, :incorrect_guesses
 
   def initialize
     @codeword = gen_codeword()
-    @alphabet = gen_alpha_set()
+    # use Sets for faster lookup
+    @alphabet = Set.new | ('a'..'z')
     @guessed = Set.new
-
+    @incorrect_guesses = 0
   end
 
   def gen_codeword
@@ -15,18 +17,23 @@ class Game
     words = File.readlines('assets/nouns.txt').map do |line|
       line.split.map(&:to_s)
     end
-    
     # randomly choose the codeword from the words array
     return words.sample
   end
 
-  def gen_alpha_set
-    set = Set.new
-    return set | ('a'..'z')
-  end
-
   def render
-    puts @codeword
+    puts ""
+    puts "UFO: The Game"
+    puts "Instructions: save us from alien abduction by guessing letters in the codeword."
+    puts ""
+    puts "UFO TEXT"
+
+    puts UFO::DISPLAY[0]
+    # render the UFO
+
+    # puts @codeword
+    # puts @alphabet
+    # puts @guessed
   end
   
 end
