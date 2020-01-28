d601af9d2bf3414789ef48f1e3e1808052c7b322
Abhi Gupta <abhifgupta@gmail.com>
2020-01-28 11:23:22 -0800

    refactor core logic for render method - about 50% functional

--- game.rb
+++ game.rb
@@ -2,56 +2,91 @@ require 'set'
 require_relative 'assets/ufo'
 
 class Game
-  attr_reader :codeword, :alphabet, :guessed, :incorrect_guesses
+  attr_reader :codeword, :alphabet, :guessed, :incorrect_guesses, :curr_guess, :char_indices, :codeword_display
 
   def initialize
     @codeword = gen_codeword()
     # use Sets for faster lookup
-    @alphabet = Set.new | ('a'..'z')
+    @alphabet = Set.new | ("a".."z")
     @guessed = Set.new
     @incorrect_guesses = 0
+    @curr_guess = ""
+    @char_indices = create_char_indices()
+    @codeword_display = codeword_key()
   end
 
   def gen_codeword
     # convert nouns.txt to an array of strings
     words = []
-    File.readlines('assets/nouns.txt').map do |line|
-      words << line
+    File.readlines("assets/nouns.txt").map do |word|
+      word.delete!("\n")
+      words << word
     end
     # randomly choose the codeword from the words array
     return words.sample
   end
 
   def codeword_key
-    key = ""
-    (@codeword.length - 1).times do
-      key += "_ "
+    (@codeword.length).times.map { "_" }
+  end
+
+  def create_char_indices
+    obj = {}
+    
+    @codeword.each_char.with_index do |char, idx|
+      if obj[char]
+        obj[char] << idx
+      else
+        obj[char] = [idx]
+      end
     end
-    return key
+
+    return obj
+  end
+
+  def is_guess_correct?
+    if @codeword.include?(@curr_guess) && !@guessed.include?(@curr_guess)
+      return true
+    else
+      return false
+    end
   end
 
   def render
-    if @incorrect_guesses < 1 && @guessed.empty?
-      puts 
-      puts "UFO: The Game"
-      puts "Instructions: save us from alien abduction by guessing letters in the codeword."
-      puts 
+    while (@incorrect_guesses <= 5 && @codeword_display.include?("_"))
+      if @curr_guess == ""
+        puts "UFO: The Game"
+        puts "Instructions: save us from alien abduction by guessing letters in the codeword."
+      elsif is_guess_correct?()
+        puts "Correct! You're closer to cracking the codeword."
+      else
+        puts @incorrect_guesses
+        puts "Incorrect! The tractor beam pulls the person in further."
+      end
+      
       puts UFO::DISPLAY[@incorrect_guesses]
-      puts
       puts "Incorrect Guesses:"
-      puts "None"
+      # puts need to display all incorrect guesses in order of guess
       puts
       puts "Codeword:"
-      puts codeword_key()
-      puts
-    else
-
+      puts @codeword_display.join(" ")
+      puts @codeword_display.length
+      puts @codeword
+      
+      puts "Please enter your guess: "
+      @curr_guess = gets.chomp
+      # validate guess
+      
+      # check if guess is correct
+      if !is_guess_correct?()
+        @incorrect_guesses += 1
+      end
     end
 
-    puts @codeword
-    puts
-    # puts @alphabet
-    # puts @guessed
+    puts UFO::DISPLAY[@incorrect_guesses]
+    puts "GAME OVER!"
+    # play again? 
+
   end
   
 end
