05881ab69d596f1c7cab1ae5c9e8318a684e5739
Abhi Gupta <abhifgupta@gmail.com>
2020-01-28 13:25:15 -0800

    refactor game class -- organize and rename methods -- main game functionality is done

--- game.rb
+++ game.rb
@@ -2,23 +2,22 @@ require 'set'
 require_relative 'assets/ufo'
 
 class Game
-  attr_reader :codeword, :alphabet, :guessed, :incorrect_guesses, 
-    :incorrect_guess_count, :curr_guess, :char_indices, :codeword_hash
+  attr_reader :codeword, :codeword_char_idxs, :codeword_hash, :alphabet,
+    :curr_guess, :guessed, :incorrect_guesses, :input_error 
 
   def initialize
-    @codeword = gen_codeword()
-    # use Sets for faster lookup
+    @codeword = select_codeword()
+    @codeword_char_idxs = create_codeword_char_idxs()
+    @codeword_hash = create_codeword_hash()
     @alphabet = Set.new | ("A".."Z")
+    @curr_guess = ""
     @guessed = Set.new
     @incorrect_guesses = []
-    @incorrect_guess_count = 0
-    @curr_guess = ""
-    @char_indices = create_char_indices()
-    @codeword_hash = codeword_key()
-    @error_msg = ""
+    @input_error = ""
   end
 
-  def gen_codeword
+  ## CODEWORD METHODS ##
+  def select_codeword
     # convert nouns.txt to an array of strings
     words = []
     File.readlines("assets/nouns.txt").map do |word|
@@ -29,60 +28,31 @@ class Game
     return words.sample
   end
 
-  def codeword_key
-    (@codeword.length).times.map { "_" }
-  end
-
-  def create_char_indices
+  def create_codeword_char_idxs
     obj = {}
     
     @codeword.each_char.with_index do |char, idx|
-      if obj[char]
-        obj[char] << idx
-      else
-        obj[char] = [idx]
-      end
+      obj[char] ? obj[char] << idx : obj[char] = [idx]
     end
 
     return obj
   end
 
-  def update_codeword_hash
-    idxs = @char_indices[@curr_guess]
-    idxs.each { |idx| @codeword_hash[idx] = @curr_guess }
+  def create_codeword_hash
+    (@codeword.length).times.map { "_" }
   end
-
-  def is_guess_correct?
-    @guessed.add(@curr_guess)
-    if @codeword.include?(@curr_guess)
-      update_codeword_hash()
-      return true
-    else
-      @incorrect_guess_count += 1
-      @incorrect_guesses << @curr_guess
-      return false
-    end
-  end
-
-  def guess_is_invalid?()
-    if @guessed.include?(@curr_guess)
-      @error_msg = "You can only guess that letter once, please try again."
-      return true
-    elsif !@alphabet.include?(@curr_guess) || @curr_guess.length != 1
-      @error_msg = "I cannot understand your input. Please guess a single letter."
-      return true
-    else
-      return false
-    end
-  end
+  ####
 
   def display_title_instrux
+    puts
     puts "UFO: The Game"
+    puts
     puts "Instructions: save us from alien abduction by guessing letters in the codeword."
   end
 
   def display_ufo
-    puts UFO::DISPLAY[@incorrect_guess_count]
+    puts UFO::DISPLAY[@incorrect_guesses.length]
+    puts
   end
 
   def display_incorrect_guesses()
@@ -92,8 +62,14 @@ class Game
   end
 
   def display_codeword_hash
-    puts "Codeword:"
+    puts "Codeword: "
     puts @codeword_hash.join(" ")
+    puts
+  end
+  
+  def update_codeword_hash
+    idxs = @codeword_char_idxs[@curr_guess]
+    idxs.each { |idx| @codeword_hash[idx] = @curr_guess }
   end
 
   def prompt_user
@@ -101,28 +77,57 @@ class Game
     @curr_guess = gets.chomp.upcase
   end
 
-  def render
-    while (@incorrect_guess_count < 6 && @codeword_hash.include?("_"))
+  def display_input_error
+    puts
+    puts @input_error
+    puts
+  end
+
+  def guess_is_invalid?()
+    if @guessed.include?(@curr_guess)
+      @input_error = "You can only guess that letter once, please try again."
+      return true
+    elsif !@alphabet.include?(@curr_guess) || @curr_guess.length != 1
+      @input_error = "I cannot understand your input. Please guess a single letter."
+      return true
+    else
+      return false
+    end
+  end
+
+  def is_guess_correct?
+    @guessed.add(@curr_guess)
+    
+    if @codeword.include?(@curr_guess)
+      update_codeword_hash()
+      puts
+      puts "Correct! You're closer to cracking the codeword."
+    else
+      @incorrect_guesses << @curr_guess
+      puts
+      puts "Incorrect! The tractor beam pulls the person in further."
+    end
+  end
+
+  def play
+    while (@incorrect_guesses.length < 6 && @codeword_hash.include?("_"))
       display_title_instrux() if @curr_guess == ""
     
       display_ufo()
       display_incorrect_guesses()
       display_codeword_hash()
-      puts @codeword
+      
+      puts @codeword #remove this line
 
       prompt_user()
       # user will be caught in a loop until they enter a valid guess
       while guess_is_invalid?()
-        puts @error_msg
+        display_input_error()
         prompt_user()
       end
 
       # check if guess is correct, display appropriate msg
-      if is_guess_correct?()
-        puts "Correct! You're closer to cracking the codeword."
-      else
-        puts "Incorrect! The tractor beam pulls the person in further."
-      end
+      is_guess_correct?()
 
     end
 
@@ -135,8 +140,7 @@ class Game
   
 end
 
-
 # starts a new game when <ruby game.rb> is run in CLI
 if $PROGRAM_NAME == __FILE__
-  Game.new.render
+  Game.new.play
 end
